120aa7db8e4d21e79b15155f3ec9818dfc8168a3
Salem Alswil <sa.al9wel@gmail.com>
2025-10-28 02:23:22 +0300

    test animation

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -38,12 +38,12 @@ const Hero = ({ border, handleNavBorder }) => {
                     </motion.div>
                     {/* pargarhs lg screens */}
                     < motion.div initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.8, duration: 1, ease: "backOut" }} className='hidden lg:flex relative text-title2 justify-center font-light items-center gap-4 lg:mt-8 xl:mt-4' >
-                        <p className='max-w-100 xl:max-w-130 xl:text-[22px] text-black/70 px-2 font-Saira font-light  bg-background2 uppercase'>I’m a creative <span className='bg-info1 text-background2 mt-1 border shadow shadow-black border-black/40 px-1 inline-block'>FRONTEND</span> Developer with a deep love for  design  and clean<span className='bg-info2 text-black/70 mb-1 mx-1 border shadow shadow-black border-black/40 px-1 inline-block'>code.</span></p>
+                        <p className='max-w-100 xl:max-w-130 xl:text-[22px] text-black/70 px-2 font-Saira font-light  bg-background2 uppercase'>I’m a creative <motion.span initial={{ opacity: 0, }} animate={{ opacity: 1, }} transition={{ delay: 1.2, duration: 0.5, ease: "backOut" }} className='bg-info1 text-background2 mt-1 border shadow shadow-black border-black/40 px-1 inline-block'>FRONTEND</motion.span> Developer with a deep love for  design  and clean<motion.span initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1.4, duration: 0.5, ease: "backOut" }} className='bg-info2 text-black/70 mb-1 mx-1 border shadow shadow-black border-black/40 px-1 inline-block'>code.</motion.span></p>
                         <p className='max-w-100 xl:max-w-130 xl:text-[18px] text-background2 font-Saira'>With every line of code, I aim to make the web more engaging, accessible, and enjoyable for everyone. Let’s create something amazing together.</p>
                         <a href="#" className='text-title1 absolute top-18 right-15 border-b border-title1 hover:text-title2 hover:border-title2 duration-200'>GET IN TOUCH {"->"}</a>
                     </motion.div >
                     < motion.div initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1, duration: 1, ease: "backOut" }} className='lg:hidden relative flex justify-start items-center flex-col p-4 gap-4 mt-4' >
-                        <p className='max-w-[600px] text-background2 uppercase font-Saira leading-7 '>Hi there! I’m a passionate <span className='bg-info1/70 px-1 text-background2'>FrontEnd</span> Developer  who loves <span className='bg-info2/70 px-1  text-background2'> crafting </span> interactive  modern , and visually <span className='bg-info4/70 text-background2 px-2'>appealing</span>  websites</p>
+                        <p className='max-w-[600px] text-background2 uppercase font-Saira leading-7 '>Hi there! I’m a passionate <motion.span initial={{ opacity: 0, }} animate={{ opacity: 1, }} transition={{ delay: 1.2, duration: 0.5, ease: "backOut" }} className='bg-info1/70 px-1 text-background2'>FrontEnd</motion.span> Developer  who loves <motion.span initial={{ opacity: 0, }} animate={{ opacity: 1, }} transition={{ delay: 1.4, duration: 0.5, ease: "backOut" }} className='bg-info2/70 px-1  text-background2'> crafting </motion.span> interactive  modern , and visually <motion.span initial={{ opacity: 0, }} animate={{ opacity: 1, }} transition={{ delay: 1.6, duration: 0.5, ease: "backOut" }} className='bg-info4/70 text-background2 px-2'>appealing</motion.span>  websites</p>
                         <a href="#" className='text-title1 self-end border-b border-title1 hover:text-title2 hover:border-title2 duration-200'>GET IN TOUCH {"->"}</a>
                     </motion.div >
                     {/* img */}
@@ -75,7 +75,7 @@ const Hero = ({ border, handleNavBorder }) => {
                             <p className=' text-center text-[10px] md:text-xs lg:p-1 xl:p-2'>ALINING YOUR BUISINESS & USER NEEDS</p>
                         </motion.div>
                     </motion.div >
-                    <motion.div class="relative mt-5 sm:mt-10 h-1 lg:mt-5 xl:mt-2   w-full bg-gradient-to-r from-transparent via-info2 to-transparent">
+                    <motion.div initial={{ opacity: 0, x: 50 }} animate={{ opacity: 1, x: 0 }} transition={{ delay: 1.2, duration: 2, ease: "backOut" }} class="relative mt-5 sm:mt-10 h-1 lg:mt-5 xl:mt-2   w-[70%] mx-auto bg-gradient-to-r from-transparent via-info2 to-transparent">
                         <div class="absolute inset-0 opacity-50 blur-lg"></div>
                     </motion.div>
                 </motion.div >
